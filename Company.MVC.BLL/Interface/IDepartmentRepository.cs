eb11e83c9ad7363cda6f37598bda2db8fa412927
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-03-10 01:56:26 +0200

    Department Module BLL

---
+++ Company.MVC.BLL/Interface/IDepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Company.MVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.MVC.BLL.Interface
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetALL();
+        Department? Get(int id);
+        int Add(Department model); 
+        int Update(Department model);   
+        int Delete(Department model);
+
+
+
+
+
+    }
+}
---
+++ Company.MVC.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,46 @@
+﻿using Company.MVC.BLL.Interface;
+using Company.MVC.DAL.Data.Context;
+using Company.MVC.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.MVC.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentRepository ()
+        {
+            _context = new CompanyDbContext (); 
+        }
+        public IEnumerable<Department> GetALL()
+        {
+            return _context.Departments.ToList();
+        }
+        public Department? Get(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+    }
+}
