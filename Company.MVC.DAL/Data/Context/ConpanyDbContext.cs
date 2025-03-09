f76f4b57ce4f99233824da5627cd86ca27e79594
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-03-10 00:22:30 +0200

    implementation of DLL

---
+++ Company.MVC.DAL/Data/Context/ConpanyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Company.MVC.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.MVC.DAL.Data.Context
+{
+    internal class ConpanyDbContext : DbContext
+    {
+        public ConpanyDbContext() : base()
+        { 
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());    
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-LA0JIU9\\MSSQLSERVER01 ; Database=CompanyMVC;Trusted_Connection=true;trustSeverCertificate=true");
+
+
+        }
+
+        public DbSet<Department>  Departments { get; set; }
+        public Assembly Assebly { get; private set; }
+    }
+}
