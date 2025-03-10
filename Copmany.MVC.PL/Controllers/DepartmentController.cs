774ebe32268e2936f3b3561b57e9e93d8a9d9a03
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-03-10 06:13:40 +0200

    Creat  create(Action-View)

--- Copmany.MVC.PL/Controllers/DepartmentController.cs
+++ Copmany.MVC.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.MVC.BLL.Interface;
 using Company.MVC.BLL.Repositories;
+using Company.MVC.DAL.Models;
+using Copmany.MVC.PL.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Copmany.MVC.PL.Controllers
@@ -18,6 +20,32 @@ namespace Copmany.MVC.PL.Controllers
         {
             var departments = _departmentRepository.GetALL();
             return View(departments);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var department = new Department 
+                {
+                    Name = model.Name,
+                    Code = model.Code,
+                    CreateAt=model.CreateAt 
+                };
+                var Count= _departmentRepository.Add(department);
+                if (Count > 0)
+                {
+                    return RedirectToAction(nameof (Index));
+                }
+            }
+            return View();
         }
     }
 }
