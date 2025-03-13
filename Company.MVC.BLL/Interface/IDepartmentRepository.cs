cee3eff1be26580aa4dc4262e1a598662cec5cde
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-03-13 06:53:20 +0200

    GenericRepository

--- Company.MVC.BLL/Interface/IDepartmentRepository.cs
+++ Company.MVC.BLL/Interface/IDepartmentRepository.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 
 namespace Company.MVC.BLL.Interface
 {
-    public interface IDepartmentRepository
+    public interface IDepartmentRepository :IGenerucRepository<Department>
     {
-        IEnumerable<Department> GetALL();
-        Department? Get(int id);
-        int Add(Department model); 
-        int Update(Department model);   
-        int Delete(Department model);
+        //IEnumerable<Department> GetALL();
+        //Department? Get(int id);
+        //int Add(Department model);
+        //int Update(Department model);
+        //int Delete(Department model);
 
 
 
--- Company.MVC.BLL/Repositories/DepartmentRepository.cs
+++ Company.MVC.BLL/Repositories/DepartmentRepository.cs
@@ -9,38 +9,10 @@ using System.Threading.Tasks;
 
 namespace Company.MVC.BLL.Repositories
 {
-    public class DepartmentRepository :IDepartmentRepository
+    public class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
     {
-        private readonly CompanyDbContext _context;
-
-        public DepartmentRepository (CompanyDbContext context)
+        public DepartmentRepository(CompanyDbContext context) : base(context)
         {
-            _context = context; 
-        }
-        public IEnumerable<Department> GetALL()
-        {
-            return _context.Departments.ToList();
-        }
-        public Department? Get(int id)
-        {
-            return _context.Departments.Find(id);
-        }
-
-        public int Add(Department model)
-        {
-            _context.Departments.Add(model);
-            return _context.SaveChanges();
-        }
-        public int Update(Department model)
-        {
-            _context.Departments.Update(model);
-            return _context.SaveChanges();
-        }
-
-        public int Delete(Department model)
-        {
-            _context.Departments.Remove(model);
-            return _context.SaveChanges();
         }
     }
 }
