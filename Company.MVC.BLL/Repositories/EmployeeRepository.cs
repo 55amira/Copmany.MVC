cdb780f08c8e83c5314efea0292a55a9d6159866
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-04-03 03:09:20 +0200

    implement search

--- Company.MVC.BLL/Repositories/EmployeeRepository.cs
+++ Company.MVC.BLL/Repositories/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Company.MVC.BLL.Interface;
 using Company.MVC.DAL.Data.Context;
 using Company.MVC.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,17 @@ namespace Company.MVC.BLL.Repositories
 {
     public class EmployeeRepository : GenericRepository<Employee>, IEmployeeRepository
     {
+        private readonly CompanyDbContext _context;
+
         public EmployeeRepository(CompanyDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public List<Employee> GetName(string name)
+        {
+           // return _context.Employees.Where(E => E.Name.ToLower() .Contains(name.ToLower())).ToList();
+               return _context.Employees.Include(E=> E.Department).Where(E => E.Name.ToLower().Contains(name.ToLower())).ToList();
         }
     }
 }
